c31e9b49519e3ac6fc697e67a4e5c7b82c67b5ae
Felipe Lugokinski <128058079+Felipaodoscompiuter@users.noreply.github.com>
2024-11-18 11:41:29 -0300

    Add files via upload

--- Clonador de Senha/main.js
+++ Clonador de Senha/main.js
@@ -1,6 +1,23 @@
-const numeroSenha = document.querySelector('.parametro-senha-texto')
-let tamanhoSenha = 50;
+const numeroSenha = document.querySelector('.parametro-senha-texto');
+let tamanhoSenha = 4;
 numeroSenha.textContent= tamanhoSenha;
-const botoes= document.querySelector ('.parametro-senha-botoes')
-botoes[0].onclick= diminuirTamanho;
+const botoes = document.querySelectorAll('.parametro-senha-botao');
+
+botoes[0].onclick = diminuirTamanho;
+botoes[1].onclick = aumentarTamanho;
+
+function diminuirTamanho(){
+    if (tamanhoSenha>3){
+    tamanhoSenha= tamanhoSenha -1
+    numeroSenha.textContent= tamanhoSenha;
+    }
+}
+
+function aumentarTamanho(){
+    if (tamanhoSenha<9){
+    tamanhoSenha=tamanhoSenha +1;
+    numeroSenha.textContent= tamanhoSenha; 
+    }
+}
+
 
