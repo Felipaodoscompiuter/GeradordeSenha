4fbdb022bf2fdbc34a0cb22dcb57881361136297
Felipe Lugokinski <128058079+Felipaodoscompiuter@users.noreply.github.com>
2024-11-25 11:43:38 -0300

    Add files via upload

--- Clonador de Senha/main.js
+++ Clonador de Senha/main.js
@@ -1,10 +1,14 @@
-const numeroSenha = document.querySelector('.parametro-senha-texto');
+const numeroSenha = document.querySelector('.parametro-senha-texto'); 
+const campoSenha = document.querySelector('#campo-senha');
+const letrasMaiusculas = "QWERTYUIOPASDFGHJKLZXCVBNMÇ";
+
 let tamanhoSenha = 4;
 numeroSenha.textContent= tamanhoSenha;
 const botoes = document.querySelectorAll('.parametro-senha-botao');
 
 botoes[0].onclick = diminuirTamanho;
 botoes[1].onclick = aumentarTamanho;
+geraSenha();
 
 function diminuirTamanho(){
     if (tamanhoSenha>3){
@@ -20,4 +24,13 @@ function aumentarTamanho(){
     }
 }
 
+function geraSenha(){
+    let senha="";
+    for (let i=0; i<tamanhoSenha; i++){
+    let numeroAleatorio = Math.random()*letrasMaiusculas.length;
+    numeroAleatorio = Math.floor(numeroAleatorio);
+    senha=senha=senha+letrasMaiusculas[numeroAleatorio];
 
+}
+campoSenha.value=senha;
+}
